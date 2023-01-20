fd5f7b007451b53ad4d661bbc78c2b2a93f39896
Michael <michael2ga3@gmail.com>
2023-01-20 10:43:38 +0300

    Fix code. Add Docker!

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# EasyAutomation_telegram
-EasyAutomation - it's very simple terminal app to auto make some action to bot's or people
+# Telegram Live Location Sender
+
+This repository contains a Python script that sends live location to Telegram.
+
+## Requirements
+- Python 3.9+
+- telethon library
+
+## Installation
+- Clone the repository
+- Run `pip install -r requirements.txt` to install the dependencies
+- Replace `YOUR_API_ID`, `YOUR_API_HASH`, `YOUR_SESSION_NAME`, `YOUR_USERNAME` and `YOUR_PHONE_NUMBER` in the `main.py` script with your own values
+
+## Usage
+- Run `python main.py` to send live location
+
+## Docker
+- Build the container using the command `docker build -t your_image_name .`
+- Run the container using the command `docker run your_image_name`
+
+## Docker compose
+- Run with docker compose using the command `docker-compose up -d --build`
+
+## Scheduling
+- Use a library such as `schedule` to run the script every 30 minutes
+
+## Authors
+- [Michael](https://github.com/michael2to3)
--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-import os
-import sys
+from telethon import types, functions
+from telethon.sync import TelegramClient
 import time
+import schedule
+from decouple import config
 
-from telethon import TelegramClient, events, utils
+api_id = config('API_ID')
+api_hash = config('API_HASH')
+session_name = config('SESSION_NAME')
+phone_number = config('PHONE_NUMBER')
+auth_code = config('AUTH_CODE')
 
+chat_id = 1928159074
 
-def get_env(name, message, cast=str):
-    if name in os.environ:
-        return os.environ[name]
-    while True:
-        value = input(message)
-        try:
-            return cast(value)
-        except ValueError as e:
-            print(e, file=sys.stderr)
-            time.sleep(1)
+client = TelegramClient('session_name', api_id, api_hash)
 
+client.start(phone_number, auth_code)
 
-session = get_env('TG_SESSION', 'Please enter your phone (or bot token):')
-api_id = int(get_env('TG_API_ID', 'Enter your API ID: '))
-api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
-
-if api_id is None or api_hash is None:
-    print("Api is None")
-    exit(1)
-
-# Create and start the client so we can make requests (we don't here)
-client = TelegramClient(session, api_id, api_hash).start()
+def send_live_location():
+    client.send_message(
+        '@echecs95',
+        file=types.InputMediaGeoLive(
+            types.InputGeoPoint(0, 0),
+            period=60
+        )
+    )
 
 
-# `pattern` is a regex, see https://docs.python.org/3/library/re.html
-# Use https://regexone.com/ if you want a more interactive way of learning.
-#
-# "(?i)" makes it case-insensitive, and | separates "options".
-@client.on(events.NewMessage(pattern=r'(?i).*\b(hello|hi)\b'))
-async def handler(event):
-    sender = await event.get_sender()
-    name = utils.get_display_name(sender)
-    print(name, 'said', event.text, '!')
+schedule.every(1).minutes.do(send_live_location)
 
-try:
-    print('(Press Ctrl+C to stop this)')
-    client.run_until_disconnected()
-finally:
-    client.disconnect()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
